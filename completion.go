883136738e6892e9988ac6886a06605dcf65bf00
Gleison Emanuel <gleison.spfc1208@hotmail.com>
2024-06-28 15:43:32 -0300

    adjusts

--- completion.go
+++ completion.go
@@ -17,6 +17,7 @@ var (
 // GPT3 Models are designed for text-based tasks. For code-specific
 // tasks, please refer to the Codex series of models.
 const (
+	GPT4o                 = "gpt-4o"
 	GPT432K0613           = "gpt-4-32k-0613"
 	GPT432K0314           = "gpt-4-32k-0314"
 	GPT432K               = "gpt-4-32k"
